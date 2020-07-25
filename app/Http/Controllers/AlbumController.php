7038b3184df27ba7a9d21462168eceab07cbe7c8
anis31 <anispurnomo31@gmail.com>
2020-07-25 23:37:36 +0700

    Tambah data FIX

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -15,4 +15,28 @@ class AlbumController extends Controller
         $album = Album::all();
         return view ('admin.album.index', compact('album', 'no'));
     }
+    
+    public function create()
+    {
+        return view('admin.album.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nama_album'=>'required',
+            'gambar'=>'required|image|mimes:jpeg,jpg,png'
+            ]);
+        $album = New Album;
+        $album->nama_album = $request->nama_album;
+        $album->album_seo = Str::slug($request->nama_album);
+
+        $gambar = $request->gambar;
+        $namafile = time().'.'.$gambar->getClientOriginalExtension();
+        $gambar->move('images/', $namafile);
+
+        $album->gambar = $namafile;
+        $album->save();
+        return redirect('/album')->with('pesan', 'Data album berhasil disimpan');
+    }
 }
