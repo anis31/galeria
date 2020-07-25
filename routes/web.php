70dba3387c7a09bafdcd1fd1a67bbac9fcf803c6
anis31 <anispurnomo31@gmail.com>
2020-07-25 22:59:34 +0700

    Add Resource Album Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('child');
 });
+
+Route::get('/album', 'AlbumController@index');
+Route::get('/album/create', 'AlbumController@create')->name('album.create');
+Route::post('/album', 'AlbumController@store')->name('album.store');
+Route::get('/album/edit/{id}', 'AlbumController@edit')->name('album.edit');
+Route::post('/album/update/{id}', 'AlbumController@update')->name('album.update');
+Route::post('/album/delete/{id}', 'AlbumController@destroy')->name('album.destroy');
